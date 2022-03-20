a9db75b3c384eef3735b5e62e3acd2eea6520d85
Probmkr <Thanatos9911@gmail.com>
2022-03-21 02:24:51 +0900

    コピペウェブサーバー

--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-console.log("Hello! Node.js x TypeScript");
+import * as http from "http";
+
+class Main {
+  constructor() {
+    // httpサーバーを設定する
+    const server: http.Server = http.createServer(
+      (request: http.IncomingMessage, response: http.ServerResponse) =>
+        this.requestHandler(request, response)
+    );
+    // サーバーを起動してリクエストを待ち受け状態にする
+    server.listen("5000");
+    // ログを出力する
+    console.log("http://localhost:5000 へアクセスください");
+  }
+
+  /**
+   * サーバーにリクエストがあった時に実行される関数
+   */
+  private requestHandler(
+    request: http.IncomingMessage,
+    response: http.ServerResponse
+  ): void {
+    response.end("Hello! Node.js with TypeScript");
+  }
+}
+
+new Main();
